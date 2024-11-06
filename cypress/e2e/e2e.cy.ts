20ce2e8bc4e2628de36bb1fa516369a2dd632172
kgaohahaha <15296612347@163.com>
2024-11-06 21:54:34 +0800

    fix: add time out

--- cypress/e2e/e2e.cy.ts
+++ cypress/e2e/e2e.cy.ts
@@ -1,12 +1,12 @@
+import { timeout } from "rxjs";
+
 describe('Send Message Test', () => {
     it('should input text and click send button', () => {
       cy.visit('/');
   
       cy.get('textarea[nz-input]').type('你好');
-  
       cy.get('button[nzType="primary"]').click();
-  
-      cy.get('.response-container p').should('contain', '你好');
+      cy.get('.response-container p',{timeout:10000}).should('contain', '你好');
     });
   });
   
